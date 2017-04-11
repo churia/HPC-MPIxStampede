b16eed22506a58a8c72f9fc0927ba2dee744a5fa
Ya Zhu <yz3768@crunchy1.cims.nyu.edu>
2017-04-10 23:19:08 -0400

    runnable

--- ssort.c
+++ ssort.c
@@ -21,8 +21,8 @@ static int compare(const void *a, const void *b)
 
 int main( int argc, char *argv[])
 {
-  int rank,root=0;
-  int i, j, N;
+  int rank,p,root=0;
+  int i, j, N, pN,rsize;
   int *vec, *sample, *rbuf, *split;
   int *scounts, *rcounts, *sdispls, *rdispls, *rray;
 
@@ -33,6 +33,7 @@ int main( int argc, char *argv[])
   /* Number of random numbers per processor (this should be increased
    * for actual tests or could be passed in through the command line */
   sscanf(argv[1], "%d", &N);
+  pN = N/p;
 
   vec = calloc(N, sizeof(int));
   /* seed random number generator differently on every core */
@@ -40,7 +41,7 @@ int main( int argc, char *argv[])
 
   /* fill vector with random integers */
   for (i = 0; i < N; ++i) {
-    vec[i] = rand();
+    vec[i] = rand()%100 +1;
   }
   printf("rank: %d, first entry: %d\n", rank, vec[0]);
 
@@ -49,9 +50,9 @@ int main( int argc, char *argv[])
 
   /* randomly sample s entries from vector or select local splitters,
    * i.e., every N/P-th entry of the sorted vector */
-  sample = (int *) calloc(N/p, sizeof(int));
-  for (i = 0; i < N/p; i++){
-    sample[i] = vec[i*p];
+  sample = (int *) calloc(pN, sizeof(int));
+  for (i = 1; i <= pN; i++){
+    sample[i-1] = vec[i*N/(pN+1)];
   }
 
   /* every processor communicates the selected entries
@@ -59,16 +60,24 @@ int main( int argc, char *argv[])
   if (rank == root){
     rbuf = (int *) calloc(N, sizeof(int));
   }
-  MPI_Gather(sample, N/p, MPI_INT, rbuf, N/p, MPI_INT, root, MPI_COMM_WORLD);
+  MPI_Gather(sample, pN, MPI_INT, rbuf, pN, MPI_INT, root, MPI_COMM_WORLD);
 
   /* root processor does a sort, determinates splitters that
    * split the data into P buckets of approximately the same size */
+  split = (int *) calloc(p-1, sizeof(int));
   if (rank == root){
     qsort(rbuf, N, sizeof(int), compare);
-    split = (int *) calloc(p-1, sizeof(int));
+    printf("rbuf\n");
+    for (i = 0; i< N; i++)
+      printf("%d ", rbuf[i]);
+    printf("\n");
     for(i = 1; i < p; i++){
-      split[i-1] = rbuf[i*p];
+      split[i-1] = rbuf[i*pN];
     }
+    printf("bcastsplit:\n");
+    for(i=0;i<p-1;i++)
+      printf("%d ",split[i]);
+    printf("\n");
   }
 
   /* root process broadcasts splitters */
@@ -76,7 +85,6 @@ int main( int argc, char *argv[])
 
   /* every processor uses the obtained splitters to decide
    * which integers need to be sent to which other processor (local bins) */
-
   scounts = (int *) calloc(p, sizeof(int));
   rcounts = (int *) calloc(p, sizeof(int));
   sdispls = (int *) calloc(p, sizeof(int));
@@ -85,12 +93,17 @@ int main( int argc, char *argv[])
   /*decide the number of data to send to each processor based on splitters*/
   j = 0;
   for(i = 0; i < N; i++){
-    if(vec[i] < split[j])
+    if(vec[i] <= split[j])
       scounts[j]++;
-    else
-      scounts[++j]++;
+    else{
+      while(vec[i] > split[j]&&j<p-1)
+	j++;
+      scounts[j]++;
+    }
   }
-
+  //for(i = 0; i < N; i++){
+  //   printf("rank %d: send %d to %d\n", rank,scounts[i],i);
+  //}
   /* tell the other processors how much data is coming */
   MPI_Alltoall(scounts, 1, MPI_INT, rcounts, 1, MPI_INT, MPI_COMM_WORLD);
   /* calculate displacements and the size of the arrays */
@@ -103,17 +116,17 @@ int main( int argc, char *argv[])
     rdispls[i] = rcounts[i-1] + rdispls[i-1];
   }
   
-  int rsize = 0;
+  rsize = 0;
   for(i = 0; i < p; i++){
     rsize += rcounts[i];
   }
   
   rray = (int *) calloc(rsize, sizeof(int));
-  MPI_Alltoallv(vec, scounts,sdisp,MPI_INT,
-                rray,rcounts,rdisp,MPI_INT, MPI_COMM_WORLD);
+  MPI_Alltoallv(vec, scounts,sdispls,MPI_INT,
+                rray,rcounts,rdispls,MPI_INT, MPI_COMM_WORLD);
 
   /* do a local sort */
-  int *temp = vec; vec = rray; rray = vec;
+  int *temp = vec; vec = rray; rray = temp;
   qsort(vec, rsize, sizeof(int), compare);
 
   /* every processor writes its result to a file */
@@ -128,8 +141,8 @@ int main( int argc, char *argv[])
     return 1;
   }
 
-  for(i = 0; i < N; ++i)
-    fprintf(fd, "%d ", vec[n]);
+  for(i = 0; i < rsize; ++i)
+    fprintf(fd, "%d ", vec[i]);
   
   fprintf(fd,"\n");
   fclose(fd);
